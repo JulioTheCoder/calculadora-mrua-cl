639b0a758ff51f5056fa75e6292887748a6c6d2f
JulioTheCoder <juljunior1@gmail.com>
2023-04-23 17:50:29 -0500

    base reconfigurado

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://github.com/JulioTheCoder/calculadora-mrua-cl/"
+  base:"https://JulioTheCoder.github.io/calculadora-mrua-cl/"
 })
