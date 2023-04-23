a318afa6573590548e972e1e2739978802ffa234
JulioTheCoder <juljunior1@gmail.com>
2023-04-22 19:48:48 -0500

    Inputs para la obtención de datos establecido

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+/* import './App.css' */
+import Form from './components/form'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [aceleracion,setAceleracion] = useState(0)
+  const [velocidadI,setVelocidadI] = useState(0)
+  const [velocidadF,setVelocidadF] = useState(0)
+  const [posicionI,setPosicionI] = useState(0)
+  const [posicionF,setPosicionF] = useState(0)
+  const [time, setTime] = useState(0)
+  const [estadoI, setEstadoI] = useState(0)
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h3>Andres manco</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form/>
     </div>
   )
 }
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,32 @@
+
+
+export default function Form(params) {
+  return(
+    <div>
+      <label>Posisión inicial
+        <input type="number"/>
+      </label>
+
+      <label>Posisión final
+        <input type="number"/>
+      </label>
+
+      <label>Aceleración
+        <input type="number"/>
+      </label>
+
+      <label>Velocidad inicial
+        <input type="number"/>
+      </label>
+
+      <label>Velocidad final
+        <input type="number"/>
+      </label>
+
+      <label>Tiempo
+        <input type="number"/>
+      </label>
+      
+    </div>
+  )
+}
