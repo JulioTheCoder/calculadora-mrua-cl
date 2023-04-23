2d99a24fab06482a8cb84226fe0ffa76114b7970
JulioTheCoder <juljunior1@gmail.com>
2023-04-23 18:01:12 -0500

    Solve error App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import Form from './components/form'
+import Form from './components/Form'
 import Suggestions from './components/Suggestions'
 import Answer from './components/Answer'
 
